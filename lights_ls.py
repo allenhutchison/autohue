17272128519c6a3d797443e60b9156320a684c0d
Allen Hutchison <allen@hutchison.org>
2015-08-30 20:02:38 -0700

    Adding additional details to listing

--- lights_ls.py
+++ lights_ls.py
@@ -9,5 +9,20 @@ config = light_config.LightConfig("testdata/lights_test.config")
 
 b = Bridge(config.GetBridgeIp())
 
-for l in b.lights:
-  print "Name:", l.name, "\tOn:",l.on
+def print_light(light):
+  if light.on:
+    label = "On"
+  else:
+    label = "Off"
+  print 'ID: {:<3}'.format(light.light_id),
+  print 'Name: {:<15}'.format(light.name),
+  print 'Status: {:<4}'.format(label),
+  print 'Brightness: {:<4}'.format(light.brightness),
+  print 'Hue: {:<3}'.format(light.hue)
+
+
+lights = b.get_light_objects('id')
+
+for l in lights:
+  print_light(lights[l])
+
